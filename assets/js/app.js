bb38e80c902e39b2605c205974103a0b8a3ed393
stopsimping <87366457+stopsimping@users.noreply.github.com>
2022-08-29 13:44:33 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -127,7 +127,7 @@ var term = $('body').terminal({
         // convert bytes to hex string                  
         const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 
-        if (hashHex != "b7200eab918db4b067d127adc58160b0df6bf1797d3c9db5d597172c7c4115a8") {
+        if (hashHex != "740ca232a5e74f7058029ea6deced21f82ee456ee476d177c4e4766df5f406b8") {
             this.echo(`${color('red', "wrong password, try again...")}`);
         }
 
